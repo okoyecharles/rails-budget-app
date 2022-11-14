802c12c31f730f724d82e3ea9de6aabe97b1e68e
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-14 13:53:05 +0100

    Migrate devise attributes to the User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :payments, class_name: 'Payment', foreign_key: 'author_id'
   has_many :categories, class_name: 'Category', foreign_key: 'author_id'
 
