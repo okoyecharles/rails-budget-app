b36e0ab17909c68ce0f0b800dd80e84b03e7d51b
OkoyeCharles <okoyecharles509@gmail.com>
2022-11-14 14:37:40 +0100

    Create splash screen page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, sign_out_via: %i[get post]
   # Defines the root path route ("/")
-  root to: "categories#index"
+  root to: 'pages#splash_screen'
+  resources :categories, only: %i[index]
 end
